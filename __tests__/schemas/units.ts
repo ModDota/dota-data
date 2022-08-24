901d46e75b4789dcb7bf6b4e1c60385c9d746f55
Shush <almogosheriko@gmail.com>
2022-08-25 00:03:47 +0300

    remove frostivus integration tests, fix paths

--- __tests__/schemas/units.ts
+++ __tests__/schemas/units.ts
@@ -5,29 +5,17 @@ describe('integration', () => {
   createIntegrationTest({
     name: 'standard units',
     schema: schemas.npc_units,
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_units.txt',
+    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_units.txt',
   });
 
   createIntegrationTest({
     name: 'standard heroes',
     schema: schemas.npc_heroes,
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/scripts/npc/npc_heroes.txt',
+    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota/pak01_dir/scripts/npc/npc_heroes.txt',
 
     ignore: [
       'npc_dota_hero_slardar.VersusScale should be a number',
       'npc_dota_hero_naga_siren.Team is not one of: "Good", "Bad"',
     ],
-  });
-
-  createIntegrationTest({
-    name: 'frostivus_2017 units',
-    schema: schemas.npc_units_custom,
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota_addons/frostivus_2017/scripts/npc/npc_units_custom.txt',
-  });
-
-  createIntegrationTest({
-    name: 'frostivus_2017 heroes',
-    schema: schemas.npc_heroes_custom,
-    url: 'https://raw.githubusercontent.com/SteamDatabase/GameTracking-Dota2/master/game/dota_addons/frostivus_2017/scripts/npc/npc_heroes_custom.txt',
   });
 });
