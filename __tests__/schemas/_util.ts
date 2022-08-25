f5af533982e53cfb1f062d4acd92281560d144c9
Perryvw <perry.wesel@gmail.com>
2022-08-25 15:37:43 +0200

    Duplicate detection & printing

--- __tests__/schemas/_util.ts
+++ __tests__/schemas/_util.ts
@@ -20,7 +20,19 @@ export function createIntegrationTest({
     const content = getFirstRoot(body);
     delete content.Version;
     parseNumbersRecursive(content);
-    ignoreDuplicates(content);
+    const duplicates = ignoreDuplicates(content);
+
+    // eslint-disable-next-line no-void
+    void duplicates;
+    // Uncomment to print out duplicate keys:
+    // if (duplicates.length > 0) {
+    //   const filteredDuplicates = duplicates.filter(
+    //     (d) => !d.includes('.Bot.Loadout.') && !d.includes('.AbilityPreview.'),
+    //   );
+
+    //   const filePath = url.split('pak01_dir/')[1] ?? url.split('master')[1];
+    //   console.log(`Found duplicate keys in: ${filePath}\n\n${filteredDuplicates.join('\n')}`);
+    // }
 
     const validationResult = schema.validateRoot(content);
     expect(validationResult).toEqual(
@@ -44,16 +56,21 @@ function parseNumbersRecursive(object: KVObject) {
   }
 }
 
-function ignoreDuplicates(object: KVObject) {
+function ignoreDuplicates(object: KVObject, path?: string): string[] {
+  const duplicates = [];
   for (const [key, value] of Object.entries(object)) {
+    const keyPath = path ? `${path}.${key}` : key;
     if (Array.isArray(value) && isDuplicateKeyArray(value)) {
+      duplicates.push(keyPath);
       const newValue = value[value.length - 1];
       object[key] = newValue;
       if (isKvObject(newValue)) {
-        ignoreDuplicates(newValue);
+        ignoreDuplicates(newValue, keyPath);
       }
     } else if (isKvObject(value)) {
-      ignoreDuplicates(value);
+      duplicates.push(...ignoreDuplicates(value, keyPath));
     }
   }
+
+  return duplicates;
 }
